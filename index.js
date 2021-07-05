9462e6fc5979bae5f71c98cf6e4244cc62f60eaf
Stepaniuk Vadim <stepaniuk@trueconf.ru>
2021-07-05 13:26:27 +0300

    rewrite rec functions, +stable import print

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 ﻿import {readFileSync, existsSync, writeFileSync, openSync, appendFileSync} from 'fs';
-import {dirname, join} from "path";
+import {dirname, join, sep , posix} from "path";
+
+
+/**
+ * Usage: node index.js './typedoc/package.json'  
+ */
+
 // utils
 /**
  *  @param {Map} map
@@ -13,8 +19,9 @@ function collectDepsFromPackageJson(map, pathToPackageJson) {
     const packageJsonContentAsJson = JSON.parse(packageJsonContent)
     
     const deps = packageJsonContentAsJson['dependencies']
-    const depsNotFound = !deps
-    if (depsNotFound) console.warn(`dependencies not found in ${pathToPackageJson}`)
+    
+    // const depsNotFound = !deps
+    //if (depsNotFound) throw new Error(`dependencies not found in ${pathToPackageJson}`)
     
     for (const key in deps) {
         if (!deps.hasOwnProperty(key)) continue
@@ -30,37 +37,68 @@ function tryCreateImportMapFile(pathToPackageJson) {
     const baseDir = dirname(pathToPackageJson)
     const pathToImportMapFile = join(baseDir, 'package.importmap.json')
     
-    if (!existsSync(pathToImportMapFile)) writeFileSync(pathToImportMapFile,'')
+    if (existsSync(pathToImportMapFile) && !forceMode) throw Error(`File already exist: ${pathToImportMapFile}`)
+    writeFileSync(pathToImportMapFile,'')
     
     return pathToImportMapFile
 }
 
 /**
- *  @param {Map} mapWithDeps
- *  @param {PathLike | String} pathToPackageJson
- */
-function mapDepsToImportMap(mapWithDeps, pathToPackageJson) {
-    const pathToImportMapFile = tryCreateImportMapFile(pathToPackageJson)
-    const desc = openSync(pathToImportMapFile,'a+')
-    
-    appendFileSync(desc,`{\n`)
-    appendFileSync(desc,`"imports": {\n`)
-    
+*  @param {Map} mapWithDeps
+*  @param {PathLike | String} pathToPackageJson
+*/
+function depsToNodeModules(pathToPackageJson, mapWithDeps){
     for (const key in mapWithDeps) {
         if (!mapWithDeps.hasOwnProperty(key)) continue
-        appendFileSync(desc, `"${key}": "/node_modules/src/etc",\n`)
+        depToNodeModules(pathToPackageJson, key)
     }
+}
 
-    appendFileSync(desc,`}\n`)
-    appendFileSync(desc,`}`)
+/**
+*  @param {String} dep
+*  @param {PathLike | String} pathToPackageJson
+*/
+function depToNodeModules(pathToPackageJson, dep) {
+    const pathToDep = join('/node_modules/', dep)
+    const pathDepToPackageJson = join(pathToDep, 'package.json')
+    
+    const pathToDepPosixLike = pathToDep.split(sep).join(posix.sep)
+    appendFileSync(fd, `"${dep}" : "${pathToDepPosixLike}",\n`);
+    
+    const fullPathToPackageJson = join (dirname(baseUrlPathArg), pathDepToPackageJson);
+    
+    const _map = new Map();
+    collectDepsFromPackageJson(_map, fullPathToPackageJson )
+    depsToNodeModules(pathDepToPackageJson, _map)
 }
 
 // exec
 const userArgs = process.argv.slice(2)
+
+// base url: 'example-project/package.json'
 const baseUrlPathArg = userArgs[0]
 const baseUrlPathNotFound = !baseUrlPathArg
 if (baseUrlPathNotFound) console.error("Argument not found: base url path to sever not found")
 
+// force mode: true / false
+const forceMode = userArgs[1] === 'true' || false
 const map = new Map();
-collectDepsFromPackageJson(map, baseUrlPathArg);
-mapDepsToImportMap(map, baseUrlPathArg)
+
+const pathToImportMapFile = tryCreateImportMapFile(baseUrlPathArg)
+const fd = openSync(pathToImportMapFile,'a+'); // file descriptor
+
+try {
+
+    appendFileSync(fd,`{\n`)
+    appendFileSync(fd,`"imports": {\n`)
+    
+    collectDepsFromPackageJson(map, baseUrlPathArg);
+    // mapDepsToImportMap(map, baseUrlPathArg)
+    depsToNodeModules(baseUrlPathArg, map);
+
+    appendFileSync(fd,`}\n`)
+    appendFileSync(fd,`}`)
+    
+} catch (e) {
+    console.error(e)
+}
