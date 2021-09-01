25d2455d90b7dc3d39bc532b082056d1d9f0fc66
VaskillerDev <Vaskoooo9241@gmail.com>
2021-09-01 15:03:23 +0300

    minor

--- index.ts
+++ index.ts
@@ -4,17 +4,15 @@ import generateImportMap from './src/utils/generateImportMap.js'
 /**
  * Usage: node index.js './typedoc/package.json'
  */
-
 // exec
 const userArgs = process.argv.slice(2)
 
 const baseUrlPathArg: string | undefined = userArgs[0]
-const baseUrlPathNotFound = !baseUrlPathArg
-if (baseUrlPathNotFound)
-  console.error('Argument not found: base url path to sever not found')
-
 const forceModeArg: boolean = userArgs[1] === 'true' || false
 const prefixArg: string = userArgs[2] || ''
+
+if (!baseUrlPathArg)
+  console.error('Argument not found: base url path to sever not found')
 
 const args: GenerateImportMapsArgs = {
   baseUrlPath: baseUrlPathArg,
