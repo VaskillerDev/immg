6964c8a8b970ef40e17213af8f9638b7a558f6ae
VaskillerDev <Vaskoooo9241@gmail.com>
2021-09-01 17:26:36 +0300

    +commander

--- index.ts
+++ index.ts
@@ -1,27 +1,27 @@
 ﻿#!/usr/bin/env node
+import {Command} from 'commander';
+
 import GenerateImportMapsArgs from './src/types/GenerateImportMapsArgs.js'
 import generateImportMap from './src/utils/generateImportMap.js'
-import printImportMap from './src/utils/printImportMap'
+import printImportMap from './src/utils/printImportMap.js'
+
+const cli = new Command();
+
+cli
+    .option('-b, --baseUrlPath [type]', '<string> path to root package.json', undefined)
+    .option('-f, --forceMode [type]', '<boolean> if enable - force rewrite previous importmap', false)
+    .option('-x, --prefix [type]', '<string> append prefix to path', '')
+    .parse();
 
 /**
  * Usage: node index.js './typedoc/package.json'
  */
 // exec
-const userArgs = process.argv.slice(2)
+const args: GenerateImportMapsArgs = cli.opts<GenerateImportMapsArgs>()
 
-const baseUrlPathArg: string | undefined = userArgs[0]
-const forceModeArg: boolean = userArgs[1] === 'true' || false
-const prefixArg: string = userArgs[2] || ''
-
-if (!baseUrlPathArg) {
+if (!args.baseUrlPath) {
   console.error('Argument not found: base url path to sever not found')
   process.exit(1)
-}
-
-const args: GenerateImportMapsArgs = {
-  baseUrlPath: baseUrlPathArg,
-  forceMode: forceModeArg,
-  prefix: prefixArg,
 }
 
 const importMap = generateImportMap(args)
