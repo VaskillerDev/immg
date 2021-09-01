8eef6d269d80b28bd59c33fd9a88b5312496450a
VaskillerDev <Vaskoooo9241@gmail.com>
2021-09-01 17:26:50 +0300

    minor

--- src/utils/printImportMap.ts
+++ src/utils/printImportMap.ts
@@ -1,8 +1,8 @@
-﻿import ImportMap from '../types/ImportMap.js'
-import GenerateImportMapsArgs from '../types/GenerateImportMapsArgs.js'
+﻿import GenerateImportMapsArgs from '../types/GenerateImportMapsArgs.js'
+import tryCreateImportMapFile from './tryCreateImportMapFile.js'
+import ImportMap from '../types/ImportMap.js'
+import Print from '../types/Print.js'
 import { openSync } from 'fs'
-import Print from '../types/Print'
-import tryCreateImportMapFile from './tryCreateImportMapFile'
 
 export default function printImportMap(
   args: GenerateImportMapsArgs,
